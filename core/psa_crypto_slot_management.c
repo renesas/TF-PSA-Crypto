040ee611964ebff84a130a9252feec90eef5e295
hienthehuynh <156345180+hienthehuynh@users.noreply.github.com>
2024-12-03 22:39:51 +0700

    Fixed build errors with llvm (#29)

--- core/psa_crypto_slot_management.c
+++ core/psa_crypto_slot_management.c
@@ -25,6 +25,9 @@
 #include "threading_internal.h"
 #endif
 
+#if defined (MBEDTLS_PSA_CRYPTO_ACCEL_DRV_C)
+#include "psa/crypto_accel_driver.h"
+#endif
 
 
 /* Make sure we have distinct ranges of key identifiers for distinct
